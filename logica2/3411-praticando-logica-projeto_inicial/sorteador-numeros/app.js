bdf14c5b9d81254da07eada4f1ebc915328c1384
Tiago <tiago.valim@outlook.com>
2024-06-01 09:43:13 -0300

    Adição de condições, verificar preenchimento dos campos e verificar se min é menor que max

--- logica2/3411-praticando-logica-projeto_inicial/sorteador-numeros/app.js
+++ logica2/3411-praticando-logica-projeto_inicial/sorteador-numeros/app.js
@@ -2,8 +2,8 @@
 variável quantidadeDeNumerosSorteados, o ideal é que os números não se repitam, e também não devo permitir
 que a quantidadeDeNumerosSorteados seja maior que o número máximo
 */
+let qntdNumeroSorteados = document.getElementById('quantidade')
 
-let quantidadeDeNumerosSorteados = document.getElementById('quantidade');
 let listaDeNumerosSorteados = [];
 
 function sortear() {
@@ -12,10 +12,20 @@ function sortear() {
 
 //Função que gera um número aleatório
 function gerarNumeroAleatorio() {
+    
+    //Pega os valores dos inputs e passa para float
     let min = parseFloat(document.getElementById('de').value);
     let max = parseFloat(document.getElementById('ate').value);
-    let numeroGerado = Math.floor(Math.random() * (max - min + 1) + min);
 
-    console.log(numeroGerado)
+    //Verifica se os campos estão preenchidos
+    if (isNaN(min) || isNaN(max)){
+        alert('Preencha o campo corretamente');
+    } else if (min > max) {
+        alert('O número mínino deve ser menor que o máximo.');
+    } else {
+        let numeroGerado = Math.floor(Math.random() * (max - min + 1) + min);
+
+        console.log(numeroGerado);
+    }
 
 }
