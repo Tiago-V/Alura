7ac82bf8fb585ac5426125433af8b1a009169839
Tiago <tiago.valim@outlook.com>
2024-06-03 21:56:02 -0300

    Sorteador de números concluído

--- logica2/3411-praticando-logica-projeto_inicial/sorteador-numeros/app.js
+++ logica2/3411-praticando-logica-projeto_inicial/sorteador-numeros/app.js
@@ -2,30 +2,67 @@
 variável quantidadeDeNumerosSorteados, o ideal é que os números não se repitam, e também não devo permitir
 que a quantidadeDeNumerosSorteados seja maior que o número máximo
 */
-let qntdNumeroSorteados = document.getElementById('quantidade')
-
-let listaDeNumerosSorteados = [];
-
-function sortear() {
-    return gerarNumeroAleatorio()
-}
 
 //Função que gera um número aleatório
-function gerarNumeroAleatorio() {
+
+let textoParagrafo = document.getElementById('texto_resultado');
+
+function sortear() {
+    let numeroGerado;
     
     //Pega os valores dos inputs e passa para float
+    let qntd = parseFloat(document.getElementById('quantidade').value);
     let min = parseFloat(document.getElementById('de').value);
     let max = parseFloat(document.getElementById('ate').value);
 
+    let sorteados = [];
+
     //Verifica se os campos estão preenchidos
-    if (isNaN(min) || isNaN(max)){
-        alert('Preencha o campo corretamente');
+    if (isNaN(min) || isNaN(max) || isNaN(qntd)){
+        alert('Preencha os campos corretamente');
     } else if (min > max) {
         alert('O número mínino deve ser menor que o máximo.');
+    } else if (qntd > (max - min)) {
+        alert(`A "Quantidade de números" não pode ser maior que a diferença entre o número máximo e o mínimo (${max - min})`)
     } else {
-        let numeroGerado = Math.floor(Math.random() * (max - min + 1) + min);
+        for (let i = 0; i < qntd; i++) {
+            numeroGerado = gerarNumeroAleatorio(min, max);
 
-        console.log(numeroGerado);
+            while (sorteados.includes(numeroGerado)) {
+                numero = gerarNumeroAleatorio(min, max);
+            }
+
+            sorteados.push(numeroGerado);
+        }
+        textoParagrafo.innerHTML = `Os números sorteados foram: ${sorteados}`;
+        alterarStatusBotao();
+        
+        console.log(sorteados);
     }
-
 }
+
+function gerarNumeroAleatorio(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function alterarStatusBotao(){
+    let botao = document.getElementById('btn-reiniciar');
+    if (botao.classList.contains('container__botao-desabilitado')){
+        botao.classList.remove('container__botao-desabilitado');
+        botao.classList.add('container__botao');
+    } else {
+        botao.classList.remove('container__botao');
+        botao.classList.add('container__botao-desabilitado');
+    }
+}
+
+function reiniciar(){
+    textoParagrafo.innerHTML = 'Números sorteados: nenhum ainda';
+    document.getElementById('quantidade').value = '';
+    document.getElementById('de').value = '';
+    document.getElementById('ate').value = '';
+    alterarStatusBotao();
+}
+
+
+
